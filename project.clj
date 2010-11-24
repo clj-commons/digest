c4113044279119d082103932f89ea1462ff400c6
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-23 21:10:34 -0800

    Wrong url

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject digest "1.0.0-SNAPSHOT"
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
-  :url "https://bitbucket.org/tebeka/dr-evil"
+  :url "https://bitbucket.org/tebeka/digest/src"
   :dependencies [[org.clojure/clojure "1.2.0"]])
