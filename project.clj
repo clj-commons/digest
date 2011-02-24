a6b0acd789fe4228ff42f9ca5694c320f65c8ddb
Miki Tebeka <miki.tebeka@gmail.com>
2011-02-24 14:07:26 -0800

    Right path to project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject digest "1.1.0"
+(defproject digest "1.1.1"
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
-  :url "https://bitbucket.org/tebeka/digest/src"
+  :url "https://bitbucket.org/tebeka/clj-digest/src"
   :dependencies [[org.clojure/clojure "1.2.0"]])
