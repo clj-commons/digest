671861f972f7befdbf4e6cfde005057fca1036b0
Miki Tebeka <miki.tebeka@gmail.com>
2016-06-16 08:09:13 +0300

    Depend on clojure 1.8.0

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
   :url "https://bitbucket.org/tebeka/clj-digest/src"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
