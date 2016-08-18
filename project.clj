69d1cfe24451c861c3134c49dbe366d79755629e
Miki Tebeka <miki.tebeka@gmail.com>
2016-08-18 11:38:46 +0300

    1.4.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject digest "1.4.4"
+(defproject digest "1.4.5"
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
   :url "https://bitbucket.org/tebeka/clj-digest/src"
