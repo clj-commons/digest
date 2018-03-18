adf89782bba9f888190f008ad6ccbaed28e97bc8
Miki Tebeka <miki.tebeka@gmail.com>
2018-03-18 05:47:19 +0200

    1.4.7

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject digest "1.4.6"
+(defproject digest "1.4.7"
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
   :url "https://github.com/tebeka/clj-digest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
