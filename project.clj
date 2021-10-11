a66741e471ca1acf5d8f97686aaed8737770e0ea
Erik Assum <erik@assum.net>
2021-10-11 19:15:28 +0200

    Setup project for clj-commons

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject digest "1.4.10"
+(defproject org.clj-commons/digest (or (System/getenv "PROJECT_VERSION") "1.4.10")
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
-  :url "https://github.com/tebeka/clj-digest"
+  :url "https://github.com/clj-commons/clj-digest"
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases true}]]          
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]])
